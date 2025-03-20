b1a8b9bc7f5473f3f38459bc96d78294740b912a
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-20 16:59:05 +0500

    import FuniroGalery

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -3,6 +3,7 @@ import FeaturesSection from "../components/ui/FeaturesSection";
 import ProductList from "../components/ui/ProductList";
 import Slider from "../components/ui/Slider";
 import TipsTricks from "@/components/ui/tips-tricks/TipsTricks";
+import { FuniroGallery } from "@/components/ui/FuniroGalery";
 
 const ShopPage = () => {
   return (
@@ -12,6 +13,7 @@ const ShopPage = () => {
       <ProductList />
       <Carousel />
       <TipsTricks />
+      <FuniroGallery />
     </>
   );
 };
