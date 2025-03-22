96821713cc3c3613bd8fb43c64b6a3fa5558aa9c
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-22 15:01:33 +0500

    index.tsx added for ui for export files

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,7 @@
+export { default as Carousel } from "./carousel";
+export { default as Feature } from "./feature";
+export { default as Slider } from "./slider";
+export { default as Dropdown } from "./dropdown";
+export { default as FuniroGalery } from "./funiro-galery";
+export { default as ProductList } from "./product-list";
+export { default as ProductCard } from "./product-card";
