af4adbba7b1ea2a7558880f74bc0a209caa009fc
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-23 09:34:34 +0500

    routes folder added

---
+++ src/routes/index.tsx
@@ -0,0 +1,70 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Global } from "@emotion/react";
+import GlobalStyles from "@/styles/global";
+import { Layout } from "@/components/layout";
+import { About, Chairs, Home, NotFound, Shop, Sofas, Tables } from "@/pages";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Layout>
+                  <Home />
+                </Layout>
+              </>
+            }
+          />
+          <Route
+            path="/shop"
+            element={
+              <Layout>
+                <Shop />
+              </Layout>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <Layout>
+                <About />
+              </Layout>
+            }
+          />
+          <Route
+            path="/chairs"
+            element={
+              <Layout>
+                <Chairs />
+              </Layout>
+            }
+          />
+          <Route
+            path="/tables"
+            element={
+              <Layout>
+                <Tables />
+              </Layout>
+            }
+          />
+          <Route
+            path="/sofas"
+            element={
+              <Layout>
+                <Sofas />
+              </Layout>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default AppRoutes;
