fd5723f19c33daab5b4dd00499e3f1e273846923
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-23 09:33:40 +0500

    App.tsx cleared

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalStyles from "./styles/globalStyles";
-import { Layout } from "./components/layout";
-import { About, Home, NotFound, Shop } from "./pages";
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Layout>
-                  <Home />
-                </Layout>
-              </>
-            }
-          />
-          <Route
-            path="/shop"
-            element={
-              <Layout>
-                <Shop />
-              </Layout>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <Layout>
-                <About />
-              </Layout>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AppRoutes />
     </>
   );
 }
