d53232335027446000923f7d02ee30df482431b7
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-23 11:40:14 +0500

    added livingroom, badroom, office routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Global } from "@emotion/react";
 import GlobalStyles from "@/styles/global";
 import { Layout } from "@/components/layout";
-import { About, Chairs, Home, NotFound, Shop, Sofas, Tables } from "@/pages";
+import {
+  About,
+  Badroom,
+  Chairs,
+  Home,
+  LivingRoom,
+  NotFound,
+  Office,
+  Shop,
+  Sofas,
+  Tables,
+} from "@/pages";
 
 const AppRoutes = () => {
   return (
@@ -57,6 +68,30 @@ const AppRoutes = () => {
             element={
               <Layout>
                 <Sofas />
+              </Layout>
+            }
+          />
+          <Route
+            path="/livingroom"
+            element={
+              <Layout>
+                <LivingRoom />
+              </Layout>
+            }
+          />
+          <Route
+            path="/badroom"
+            element={
+              <Layout>
+                <Badroom />
+              </Layout>
+            }
+          />
+          <Route
+            path="/office"
+            element={
+              <Layout>
+                <Office />
               </Layout>
             }
           />
