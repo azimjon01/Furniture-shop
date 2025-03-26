6dbb0aeb28f05aef413c8d452061cc35fcdb352b
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-26 17:09:41 +0500

    added allData

---
+++ src/data/allData.ts
@@ -0,0 +1,15 @@
+import { bedroomData } from "@/pages/badroom/data";
+import { chairData } from "@/pages/chairs/data";
+import { livingRoomData } from "@/pages/living-room/data";
+import { officeData } from "@/pages/office/data";
+import { sofaData } from "@/pages/sofas/data";
+import { tableData } from "@/pages/tables/data";
+
+export const allData = {
+  chairs: chairData,
+  tables: tableData,
+  sofas: sofaData,
+  livingRoom: livingRoomData,
+  bedroomData: bedroomData,
+  officeData: officeData,
+};
