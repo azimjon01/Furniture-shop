8f7814e6c34db62f6fe49a920b0df2ae7d04b122
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-03-27 15:26:34 +0500

    fixed about page exported

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export { default as Welcome } from "./welcome";
-export { default as Home } from "./home";
 export { default as About } from "./about";
+export { default as Home } from "./home";
 export { default as Shop } from "./shop";
 export { default as NotFound } from "./not-found";
 export { default as Chairs } from "./chairs";
